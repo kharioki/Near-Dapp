1646e551a8d6c72ffb57d3be38ed58ae2bd56ebf
tony <antolisah@gmail.com>
2022-02-04 02:10:02 +0300

    navbar

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import { login, logout } from './utils'
 import './global.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
 
+import Logo from './assets/DeCash.svg';
+
 import { Navbar, Container, Nav, NavDropdown } from 'react-bootstrap'
 
 import getConfig from './config'
@@ -14,20 +16,15 @@ export default function App() {
     <>
       <Navbar bg="light" expand="lg">
         <Container>
-          <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+          <Navbar.Brand href="#home">
+            <img src={Logo} />
+          </Navbar.Brand>
+
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="me-auto">
-              <Nav.Link href="#home">Home</Nav.Link>
-              <Nav.Link href="#link">Link</Nav.Link>
-              <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-                <NavDropdown.Divider />
-                <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
-              </NavDropdown>
-            </Nav>
+            <Nav className="me-auto"></Nav>
+            <Nav.Link>Transactions</Nav.Link>
+            <Nav.Link>Login</Nav.Link>
           </Navbar.Collapse>
         </Container>
       </Navbar>
